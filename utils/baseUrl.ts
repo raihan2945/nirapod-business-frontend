59fd2e8ef61b297eee036545842eddc741109b56
raihan2945 <000raihanahmad@gmail.com>
2025-12-06 14:06:32 +0600

    fix :issue fixed

--- utils/baseUrl.ts
+++ utils/baseUrl.ts
@@ -1,2 +1,2 @@
-// export const baseUrl = "http://159.223.53.146:4001"
-export const baseUrl = "http://localhost:4001"
+export const baseUrl = "http://159.223.53.146:4001"
+// export const baseUrl = "http://localhost:4001"
