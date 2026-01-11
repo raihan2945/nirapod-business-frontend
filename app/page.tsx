0beb4b400f0781e69a6fb5a4e6813ca3c3f8c754
raihan2945 <000raihanahmad@gmail.com>
2026-01-12 00:09:26 +0600

    fix: app updated

--- app/page.tsx
+++ app/page.tsx
@@ -35,7 +35,7 @@ export default function Home({
         <ProjectSection />
         <StatisticsSection />
         {/* <FAQSection /> */}
-        <FeedbackSection />
+        {/* <FeedbackSection /> */}
         <BlogSection />
         <NewsletterSection />
         <Footer />
